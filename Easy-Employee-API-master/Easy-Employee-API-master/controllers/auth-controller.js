32d83c15bf2c66511c3e0c560c90a222d51aa804
unknown <bibhudattarw@gmail.com>
2025-01-10 18:44:35 +0530

    sfdvefvefv

--- Easy-Employee-API-master/Easy-Employee-API-master/controllers/auth-controller.js
+++ Easy-Employee-API-master/Easy-Employee-API-master/controllers/auth-controller.js
@@ -91,11 +91,13 @@ class AuthController {
 
     refresh = async (req,res,next) =>
     {
-        console.log('hahahahahhahahaha',refreshToken,req.cookies);
-        if(!refreshToken) return next(ErrorHandler.unAuthorized());
-        const userData = await tokenService.verifyRefreshToken(refreshToken);
+        console.log('req.ffffff',req.cookies)
+        const {refreshToken:refreshTokenFromCookie} = req.cookies;
+        console.log('hahahahahhahahaha',refreshTokenFromCookie,req.cookies);
+        if(!refreshTokenFromCookie) return next(ErrorHandler.unAuthorized());
+        const userData = await tokenService.verifyRefreshToken(refreshTokenFromCookie);
         const {_id,email,username,type} = userData;
-        const token = await tokenService.findRefreshToken(_id,refreshToken);
+        const token = await tokenService.findRefreshToken(_id,refreshTokenFromCookie);
         if(!token)
         {
             res.clearCookie('refreshToken');
