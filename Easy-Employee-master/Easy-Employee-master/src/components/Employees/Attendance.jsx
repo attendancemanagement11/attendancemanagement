0bba58af421cc6dcc5996f23d1d10e03abcff08d
unknown <bibhudattarw@gmail.com>
2025-01-13 12:37:01 +0530

    kkkllll

--- Easy-Employee-master/Easy-Employee-master/src/components/Employees/Attendance.jsx
+++ Easy-Employee-master/Easy-Employee-master/src/components/Employees/Attendance.jsx
@@ -18,7 +18,7 @@ const Attendance = () => {
   const [selectedDay, setSelectedDay] = useState('');
   const [attendance, setAttendance] = useState();
 
-  const years = [2020,2021,2022, 2023, 2024]; // Customize this as needed
+  const years = [2020,2021,2022, 2023, 2024,2025,2026]; // Customize this as needed
   const months = [
     'January', 'February', 'March', 'April', 'May', 'June',
     'July', 'August', 'September', 'October', 'November', 'December'
