9e7e2be97b06980b8fbaf762602ca69edb1bc14f
unknown <bibhudattarw@gmail.com>
2025-01-13 12:13:15 +0530

    check

--- Easy-Employee-API-master/Easy-Employee-API-master/configs/mail-config.js
+++ Easy-Employee-API-master/Easy-Employee-API-master/configs/mail-config.js
@@ -1,10 +1,10 @@
 const nodeMailer = require('nodemailer');
-const smtpHost = process.env.SMTP_HOST;
-const smtpPort = process.env.SMTP_PORT; 
-const smtpSecure = process.env.SMTP_SECURE;
-const smtpRequireTLS = process.env.SMTP_REQUIRE_TLS;
-const smtpAuthUser = process.env.SMTP_AUTH_USER;
-const smtpAuthPass = process.env.SMTP_AUTH_PASS;
+const smtpHost = process.env.SMTP_HOST || 'smtp.gmail.com';
+const smtpPort = process.env.SMTP_PORT || 587; 
+const smtpSecure = process.env.SMTP_SECURE ? JSON.parse(process.env.SMTP_SECURE) : true; // Convert 'true'/'false' string to boolean
+const smtpRequireTLS = process.env.SMTP_REQUIRE_TLS ? JSON.parse(process.env.SMTP_REQUIRE_TLS) : true;
+const smtpAuthUser = process.env.SMTP_AUTH_USER || 'uccbibhu12321@gmail.com';
+const smtpAuthPass = process.env.SMTP_AUTH_PASS || 'ryvoulcduolpoklp';
 
 transpoter = nodeMailer.createTransport({
     host:smtpHost,
